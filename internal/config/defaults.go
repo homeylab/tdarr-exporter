1a0740f7f42be44b9e69fe394337d0e48b8145a7
pchang388 <pchang388@gmail.com>
2023-12-18 05:56:57 -0500

    wip

--- internal/config/defaults.go
+++ internal/config/defaults.go
@@ -1,23 +1,11 @@
 package config
 
-// import (
-// 	"github.com/homeylab/tdarr-exporter/internal/config"
-// )
-
-const (
-	defaultListenPort         = "9090"
-	defaultPath               = "/metrics"
-	defaultSslVerify          = true
-	defaultLogLevel           = "info"
-	defaultHttpTimeoutSeconds = 10
-)
-
 func GetDefaults() Config {
 	return Config{
-		LogLevel:           defaultLogLevel,
-		VerifySsl:          defaultSslVerify,
-		PrometheusPort:     defaultListenPort,
-		PrometheusPath:     defaultPath,
-		HttpTimeoutSeconds: defaultHttpTimeoutSeconds,
+		LogLevel:           "info",
+		VerifySsl:          true,
+		PrometheusPort:     "9090",
+		PrometheusPath:     "/metrics",
+		HttpTimeoutSeconds: 10,
 	}
 }
